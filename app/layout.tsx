759554710b2de7dc4e21b444484ddaac53d48275
ArthurSrn <arthursornin91@gmail.com>
2023-06-13 11:07:09 +0200

    ajout de axeptio et hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,31 @@ export default function RootLayout({
 
             gtag('config', 'G-8F2W7S0C6S');
           </script>
+
+          <!-- Hotjar Tracking Code for https://commback.fr/ -->
+          <script>
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3508851,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          </script>
+
+          <script>
+              window.axeptioSettings = {
+                clientId: "648830fbc138970094bb1987",
+                cookiesVersion: "commback-fr",
+              };
+               
+              (function(d, s) {
+                var t = d.getElementsByTagName(s)[0],e = d.createElement(s);
+                e.async = true; e.src = "//static.axept.io/sdk.js";
+                t.parentNode.insertBefore(e, t);
+              })(document, "script");
+          </script>
          
         `}
       </Script>
